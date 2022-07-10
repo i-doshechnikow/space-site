62ab4eaf14139445760739b1e7e6cfe1bfb0c4be
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-07-10 22:47:22 +0300

    redesigned the category structure

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,12 +1,19 @@
 export default () => {
-  const categories = ["Hats", "Jackets", "Sneackers", "Womans", "Mens"];
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneackers" },
+    { id: 4, title: "Womans" },
+    { id: 5, title: "Mens" },
+  ];
+
   return (
     <div className="categories-container">
-      {categories.map((category) => (
-        <div className="category-container" key={category}>
+      {categories.map(({ title, id }) => (
+        <div className="category-container" key={id}>
           {/* <img /> */}
           <div className="category-body-container">
-            <h2>{category}</h2>
+            <h2>{title}</h2>
             <p>Shop now</p>
           </div>
         </div>
