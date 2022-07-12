9333e7e8f6fcb3c2a05a0dad6c6995f763ea6dbc
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-07-12 20:28:09 +0300

    transfer categoty item to another component

---
+++ shop/src/components/category-item/category-item.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./category-item.styles.scss";
+
+export default ({ category: { title, imageUrl } }) => (
+  <div className="category-container">
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop now</p>
+    </div>
+  </div>
+);
