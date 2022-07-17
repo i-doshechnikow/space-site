0a4cda0e23687d6f2a2e2f66bf838c14c841a335
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-07-17 19:16:32 +0300

    frame for feature nav

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home";
 import Directory from "./components/directory/directory";
 
 const Shop = () => <div>shop</div>;
 
+const Navigation = () => (
+  <div>
+    <div>
+      <h1>navigation bar</h1>
+    </div>
+    <Outlet />
+  </div>
+);
+
 export default () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
